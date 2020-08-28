b512407d16377fd2697b8a0d9ae1e750dc7b7849
Arkadi Koifman <arkadi29081994@gmail.com>
2020-08-28 20:06:18 +0300

    1

---
+++ js/script.js
@@ -0,0 +1,41 @@
+"use strict";
+
+window.addEventListener('DOMContentLoaded', () => {
+    const tabContent = document.querySelectorAll('.tabcontent'),
+          tabHeaderItems = document.querySelector('.tabheader__items'),
+          tabHeaderItem = document.querySelectorAll('.tabheader__item');
+
+    function hideTab() {
+        tabHeaderItem.forEach(function(item) {
+            item.classList.remove('tabheader__item_active');
+
+        });
+
+        tabContent.forEach(function(item) {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
+
+    }
+
+    function showTab(i=0) {
+        tabContent[i].classList.add('show', 'fade');
+        tabContent[i].classList.remove('hide');
+        tabHeaderItem[i].classList.add('tabheader__item_active');
+    }
+
+    tabHeaderItems.addEventListener('click', (event) => {
+        if (event.target && event.target.classList.contains('tabheader__item')) {
+            tabHeaderItem.forEach(function(item, i) {
+                if (event.target == item) {
+                    hideTab();
+                    showTab(i);
+
+                } 
+            });
+        }
+    });
+
+    hideTab();
+    showTab(); 
+});
