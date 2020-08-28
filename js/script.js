6f4741e07d60b751e9bae44fc71c3863e299d2be
Arkadi Koifman <arkadi29081994@gmail.com>
2020-08-28 21:55:53 +0300

    write the timer functionality

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 "use strict";
 
 window.addEventListener('DOMContentLoaded', () => {
+    //Tabs
     const tabContent = document.querySelectorAll('.tabcontent'),
           tabHeaderItems = document.querySelector('.tabheader__items'),
           tabHeaderItem = document.querySelectorAll('.tabheader__item');
@@ -38,4 +39,95 @@ window.addEventListener('DOMContentLoaded', () => {
 
     hideTab();
     showTab(); 
+
+    //Timer
+
+    // const deadLine = "2020-08-31";
+    // function timeLeft (endTime){
+    //     const t = Date.parse(endTime) - new Date(),
+    //           days = Math.floor(t/(1000*60*60*24)),
+    //           hours = Math.floor(t/(1000*60*60) % 24),
+    //           minutes = Math.floor(t/(1000*60) % 60),
+    //           seconds = Math.floor(t/(1000) % 60);
+        
+    //     return {
+    //         total : t,
+    //         days : days,
+    //         hours : hours,
+    //         minutes : minutes,
+    //         seconds : seconds
+    //     };
+    // }
+    // function addZero(num){
+    //     if (num >= 0 && num < 10){
+    //         return `0${num}`;
+    //     } else{
+    //         return num;
+    //     }
+    // }
+    // function timerStracture (position, endTime){
+    //     const t = document.querySelector(position),
+    //           days = t.querySelector("#days"),
+    //           hours = t.querySelector("#hours"),
+    //           minutes = t.querySelector("#minutes"),
+    //           seconds = t.querySelector("#seconds"),
+    //           timeUpdating = setInterval(updateTime,1000);
+    //     updateTime();
+
+    //     function updateTime (){
+    //         const time = timeLeft(endTime);
+    //         if (time.total <= 0){
+    //             clearInterval(timeUpdating);
+    //         } else{
+    //             days.innerHTML = addZero(time.days);
+    //             hours.innerHTML = addZero(time.hours);
+    //             minutes.innerHTML = addZero(time.minutes);
+    //             seconds.innerHTML = addZero(time.seconds);
+    //         }
+    //     }
+    // }
+    // timerStracture(".timer", deadLine);
+
+
+    const timeToFinish = "10";
+    function timeLeft (endTime){
+        const seconds = timeToFinish;
+        
+        return {
+            seconds : seconds
+        };
+    }
+    function addZero(num){
+        if (num >= 0 && num < 10){
+            return `0${num}`;
+        } else{
+            return num;
+        }
+    }
+    function timerStracture (position, endTime){
+        const t = document.querySelector(position),
+              days = t.querySelector("#days"),
+              hours = t.querySelector("#hours"),
+              minutes = t.querySelector("#minutes"),
+              seconds = t.querySelector("#seconds"),
+              time = timeLeft(endTime),
+              timeUpdating = setInterval(updateTime,1000);
+        updateTime();
+
+        function updateTime (){
+            if (time.seconds <= -1){
+                clearInterval(timeUpdating);
+            } else{
+                days.innerHTML = "";
+                hours.innerHTML = "";
+                minutes.innerHTML = "";
+                seconds.innerHTML = addZero(time.seconds);
+                time.seconds-- ;
+
+            }
+        }
+    }
+    timerStracture(".timer", timeToFinish);
+
+
 });
